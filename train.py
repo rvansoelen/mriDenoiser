93f11a967ae164cb88aa57dfcf8dfa4d1e1e4dcf
rvansoelen <vansoelen.1@osu.edu>
2018-04-23 21:52:32 -0400

    Updated train.py

--- train.py
+++ train.py
@@ -1,13 +1,26 @@
 #trains the foe model
 #This function trains the Field of Experts Model. It loads the training images, 
 #updates the FoE model, and save the model to a file (using the pickling technique)
+import foe, gmm, mapEstimator
+from scipy import ndimage
+
 def main():
+	filenames = ['outoutPlaceholder']
 	#load noisy and ground truth images
+	images = []
+	for filename in filenames:
+		images.append(ndimage.imread(filename))
 
 	#create initial foe model
+	foe = FoE()
 
 	#for each epoch (if more than one)
+	numEpochs = 10
+	for epoch in range():
 		#for each image:
+		for image in images:
 			#call training function of foe model
+			foe.train(image)
 
 	#save model to file
+	
